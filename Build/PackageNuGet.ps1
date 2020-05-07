20278b0d5fbd68eec79a6e8fe9079c5285b249a0
Ben Lipson <blipson89@gmail.com>
2020-05-06 20:45:17 -0400

    Switched packaging to dotnet pack

--- Build/PackageNuGet.ps1
+++ Build/PackageNuGet.ps1
@@ -2,31 +2,6 @@ param($scriptRoot)
 
 $ErrorActionPreference = "Stop"
 
-function Resolve-MsBuild {
-	$msb2017 = Resolve-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\*\*\MSBuild\*\bin\msbuild.exe" -ErrorAction SilentlyContinue
-	if($msb2017) {
-		Write-Host "Found MSBuild 2017 (or later)."
-		Write-Host $msb2017
-		return $msb2017
-	}
-
-	$msBuild2015 = "${env:ProgramFiles(x86)}\MSBuild\14.0\bin\msbuild.exe"
-
-	if(-not (Test-Path $msBuild2015)) {
-		throw 'Could not find MSBuild 2015 or later.'
-	}
-
-	Write-Host "Found MSBuild 2015."
-	Write-Host $msBuild2015
-
-	return $msBuild2015
-}
-
-$msBuild = Resolve-MsBuild
-$nuGet = "$scriptRoot..\tools\NuGet.exe"
-$solution = "$scriptRoot\..\Configy.sln"
-
-& $nuGet restore $solution
-& $msBuild $solution /p:Configuration=Release /t:Rebuild /m
-
-& $nuGet pack "$scriptRoot\..\src\Configy\Configy.csproj" -Symbols -Prop "Configuration=Release"
+Push-Location "$scriptRoot\..\src\Configy"
+& dotnet pack "Configy.csproj" -c "Release" -o "$scriptRoot\packages"
+Pop-Location
